58e2587cae79ebfe7e3a312fabada85664da5f9b
antonio toloza <antoniodejesustoloza@gmail.com>
2026-02-17 18:01:50 -0300

    switch,if,else

--- C1/index.js
+++ C1/index.js
@@ -1,1 +1,36 @@
-console.log("Hola")
+/*console.log("Hola como estas");
+var cansancio = true;
+if(cansancio) {
+    console.log("Nos quedamos en casita")
+}else{ 
+    console.log("Vamos a entrenar")
+}*/
+ 
+/*var lunes = false;
+var martes = false;
+var miercoles = false;
+var jueves = false;
+var viernes = false;
+if (lunes || miercoles){
+    console.log("Voy a entrenar")
+}
+if (martes||jueves){
+    console.log("Me queddo en casita")
+} else {
+    console.log("salir")
+}*/
+//switch
+var item=0;
+switch(item){
+    case 1:
+        console.log("coca");
+        break;
+    case 2:
+        console.log("Alfajor jorgito");
+        break;
+    case 3:
+        console.log("Mirinda");
+        break
+    default:
+        console.log("agua");
+}
